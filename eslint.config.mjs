f3fc860f501e37f7dfd2b4bddd95cf39fb456a25
Andri Alexandrou <andrialexandrou@github.com>
2025-01-04 01:57:36 +0000

    chore: updates eslint ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}, ignores: ["**/node_modules/**"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];
